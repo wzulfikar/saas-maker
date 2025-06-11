4d764a5076ae2965bada6aaad82401f3eb517a4f
Wildan Zulfikar <hey@wzulfikar.com>
2025-06-11 18:27:08 +0300

    add get set to devmode

--- src/client/devmode.ts
+++ src/client/devmode.ts
@@ -1,10 +1,12 @@
+const DEVMODE_KEY = 'devmode'
+
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 let _devmode: Record<string, any> = {}
 
 try {
   _devmode =
     typeof window !== 'undefined'
-      ? JSON.parse(localStorage.getItem('devmode') ?? '{}')
+      ? JSON.parse(localStorage.getItem(DEVMODE_KEY) ?? '{}')
       : {}
 } catch (e) {
   console.error(`Error parsing 'devmode' from localStorage. error: ${e}`)
@@ -14,7 +16,14 @@ try {
  * Get value from `devmode` in localStorage. You'll need to
  * reload the page after changing a value from localStorage.
  */
-export const devmode = <T>(
-  key: string,
-  defaultValue?: T,
-): T extends infer U ? U : never => _devmode?.[key] ?? defaultValue
+export const devmode = {
+  get: <T>(
+    key: string,
+    defaultValue?: T,
+  ): T extends infer U ? U : never => _devmode?.[key] ?? defaultValue,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  set: (key: string, value: any) => {
+    _devmode[key] = value
+    localStorage.setItem(DEVMODE_KEY, JSON.stringify(_devmode))
+  }
+}
